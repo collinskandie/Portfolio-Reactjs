42d04e22428d0010ddb56fcf626775ba31d2a4bd
Collins Kipruto <colllinskandie1@gmail.com>
2022-02-21 01:00:20 +0300

    before adding font awesome

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Intro from "./components/intro/Intro";
 import About from "./components/about/About";
+import ProductList from "./components/productList/Productlist";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
   return (
   <div>
   <Intro/>
   <About/>
+  <ProductList/>
+  <Contact/>
   
   </div>
   )
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,29 @@
+import "./contact.css"
+import Phone from "../../img/Phone.png"
+import Email from "../../img/email.png"
+// import Phone from "../../img/phone.pg"
+const Contact = () => {
+    return (
+        <div className="c">
+        <div className="c-bg">
+        <div className="c-wrapper">
+            <div className="c-left">
+                <h1 className="c-title">Get in touch</h1>
+                <div className="c-info">
+                    <div className="c-info-item">
+                    <i class="fa-solid fa-phone">+245722545735</i>
+                    
+                    </div>
+                </div>
+            </div>
+            <div className="c-right"></div>
+        </div>
+
+
+        </div>
+
+
+        </div>
+    )
+}
+export default Contact

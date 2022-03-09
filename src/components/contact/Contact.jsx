4c5711b85c05378d2acdb70d0a3d4c01938e3ec3
Collins Kipruto <colllinskandie1@gmail.com>
2022-03-09 12:29:48 +0300

    Added email reply

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,11 +3,24 @@ import "./contact.css"
 // import Email from "../../img/email.png"
 // import Phone from "../../img/phone.pg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faEnvelopeOpenText, faPhoneSquare} from '@fortawesome/free-solid-svg-icons'
+import {faEnvelopeOpenText, faLink, faPhoneSquare} from '@fortawesome/free-solid-svg-icons'
+import { useRef, useState } from "react"
+import emailjs from '@emailjs/browser'
 const Contact = () => {
+    const formRef= useRef();
+    const [done,setDone] = useState(false);
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        emailjs.sendForm('service_9d95q7b', 'template_9j195e4', formRef.current, 'Yek7U9wnGpr-aDa3W').then((result) => {
+            console.log(result.text);
+            setDone(true);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
     return (
         <div className="c">
-        <div className="c-bg">
+        <div className="c-bg"></div>
         <div className="c-wrapper">
             <div className="c-left">
                 <h1 className="c-title">Lets get in touch</h1>
@@ -18,15 +31,29 @@ const Contact = () => {
                     </div>
                     <div className="c-info-item">
                     <FontAwesomeIcon icon={faEnvelopeOpenText} className="c-icon"/>
-                    <a href="mailto:collinskandie1@gmail.com">collinskandie1@gmail.com</a>                   
+                    <a href="mailto:collinskandie1@gmail.com">Send an Email</a>
                     </div>
+                    <div className="c-info=item">
+                    <FontAwesomeIcon icon={faLink} className="c-icon" />
+                    <a href="https://www.linkedin.com/in/collins-kiptoo-0929b81aa/">Collins Kipruto</a>
+                    </div>
+                    
                 </div>
             </div>
-            <div className="c-right"></div>
-        </div>
-        </div>
-
-
+            <div className="c-right">
+                <p className="c-desc">
+                    <b>Whats your project?</b> Get in touch. I would like you to be my next happy and satisfied client
+                </p>
+                <form ref={formRef} onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Name" name="username"></input>
+                    <input type="text" placeholder="Email" name="email"></input>
+                    <input type="text" placeholder="Subject" name="subject"></input>
+                    <textarea rows="5" placeholder="Message" name="message"></textarea>
+                    <button>Submit</button> 
+                    {done && "Thank you for your feedback, will get back to you soon."}
+               </form>
+            </div>
+        </div>       
         </div>
     )
 }
--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -2,12 +2,7 @@ import "./product.css"
 const Product = ({img,link}) => {
     return (
         <div className="p">
-            <div className="p-browser">
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-            </div>
-            <a href={link} target="_blank" rel="noreferrer">
+                <a href={link} target="_blank" rel="noreferrer">
                 <img src={img} alt="" className="p-img"/>
             </a>
         </div>
